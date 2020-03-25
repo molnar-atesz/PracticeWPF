548d83e67e47b37278a3ca4e9b81310364feb599
ngAtesz <attila.molnar@codecool.com>
2020-03-25 11:55:15 +0100

    [WIP] Core project added.

---
+++ CovidStat.Core/CovidDataService.cs
@@ -0,0 +1,26 @@
+﻿using CovidStat.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidStat
+{
+    public class CovidDataService
+    {
+        private readonly string API_URL = @"https://coronavirus-19-api.herokuapp.com/";
+
+        public async Task<List<CountryStat>> LoadCountries()
+        {
+            var client = new HttpClient();
+            string requestUri = $"{API_URL}countries";
+            var data = await client.GetAsync(requestUri);
+            var stats = await data.Content.ReadAsStringAsync().ConfigureAwait(true);
+
+            return JsonConvert.DeserializeObject<List<CountryStat>>(stats);
+        }
+    }
+}
