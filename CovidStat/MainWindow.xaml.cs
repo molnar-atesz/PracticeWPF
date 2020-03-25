3af96e5763f20e1cb8cce8e691ec14f99c6e2bbd
ngAtesz <attila.molnar@codecool.com>
2020-03-25 11:36:00 +0100

    Add project files.

---
+++ CovidStat/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CovidStat.Models;
+using CovidStat.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CovidStat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel _vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _vm = new MainWindowViewModel();
+            DataContext = _vm;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _vm.LoadCountries();
+            _vm.SelectCountry("Hungary");
+        }
+
+    }
+}
---
+++ CovidStat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using CovidStat.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CovidStat.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly string API_URL = @"https://coronavirus-19-api.herokuapp.com/";
+        private CountryStat _currentCountry;
+        private List<CountryStat> countryList;
+
+        public CountryStat CurrentCountry
+        {
+            get => _currentCountry;
+            set
+            {
+                _currentCountry = value;
+                OnPropertyChanged(nameof(CurrentCountry));
+            }
+        }
+
+        public List<CountryStat> CountryList
+        {
+            get => countryList;
+            set
+            {
+                countryList = value;
+                OnPropertyChanged(nameof(CountryList));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            CurrentCountry = new CountryStat();
+        }
+
+        internal void SelectCountry(string countryName)
+        {
+            CurrentCountry = CountryList.Where(stat => stat.Country == countryName).FirstOrDefault();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public async Task LoadCountries()
+        {
+            var client = new HttpClient();
+            string requestUri = $"{API_URL}countries";
+            var data = await client.GetAsync(requestUri);
+            var stats = await data.Content.ReadAsStringAsync().ConfigureAwait(true);
+
+            CountryList = JsonConvert.DeserializeObject<List<CountryStat>>(stats);
+        }
+    }
+}
